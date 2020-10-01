4f7ed0a311f8deaf2373674d7992a1200d179b15
apotinvs <apotinvs@gmail.com>
2020-10-01 09:29:38 +0600

    feat: add min mas avg logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+	let arrayMin
+	if(array && array.length > 0){
+		arrayMin = array.sort(function(a, b) {
+			return a - b;
+		});
+		return arrayMin[0]
+	}
+	return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+	let arrayMax
+	if(array && array.length > 0){
+		arrayMax = array.sort(function(a, b) {
+			return b - a;
+		});
+		return arrayMax[0]
+	}
+	return 0;	
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {	
+	if(array && array.length > 0){
+		let arrayAvg = array[0]
+		for (let i = 0; i < array.length - 1; i++){
+			arrayAvg += array[i + 1]
+		}
+		return arrayAvg / array.length
+	}
+	return 0;	
 }
